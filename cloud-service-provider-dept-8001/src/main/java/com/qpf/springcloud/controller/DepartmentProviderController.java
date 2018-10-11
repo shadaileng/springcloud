c5fee09f6ee3bdeb9b31cd61922af142e7bd5016
shadaileng <qpf0510@qq.com>
2018-10-11 16:59:19 +0800

    discovery

--- cloud-service-provider-dept-8001/src/main/java/com/qpf/springcloud/controller/DepartmentProviderController.java
+++ cloud-service-provider-dept-8001/src/main/java/com/qpf/springcloud/controller/DepartmentProviderController.java
@@ -3,6 +3,8 @@ package com.qpf.springcloud.controller;
 import com.qpf.springcloud.entities.Department;
 import com.qpf.springcloud.service.DepartmentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -11,6 +13,9 @@ import java.util.List;
 public class DepartmentProviderController {
     @Autowired
     private DepartmentService service;
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
 
     @RequestMapping(value = "/dept/add", method = RequestMethod.POST)
     public boolean add(@RequestBody Department department) {
@@ -25,5 +30,21 @@ public class DepartmentProviderController {
     @GetMapping("/dept/list")
     public List<Department> list() {
         return service.list();
+    }
+
+    @GetMapping("/dept/discovery")
+    public Object discovery() {
+
+        List<String> services = discoveryClient.getServices();
+        for(String service: services) {
+            System.out.println("---- " + service);
+        }
+        // spring.application.name
+        List<ServiceInstance> instances = discoveryClient.getInstances("cloud-service-provider-dept-8001");
+        for(ServiceInstance instance: instances) {
+            System.out.println("-- " + instance.getServiceId() + "\t" + instance.getHost() + "\t" + instance.getPort() + "\t" + instance.getUri() + "\t" + instance.getMetadata());
+        }
+
+        return discoveryClient;
     }
 }
